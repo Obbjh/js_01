1905c79813e9eecc48a3b83d9c41ad3d97f9c7d7
Losev Vladyslav <135152001+Obbjh@users.noreply.github.com>
2023-09-26 13:19:33 +0300

    js

--- js/js_01.js
+++ js/js_01.js
@@ -1,3 +1,11 @@
+function makeMessage (name, price) {
+  // Change code below this line
+   const message = "You picked ${name}, price per item is ${price} credits";
+  // Change code above this line
+  return message; 
+};
 
-const pricePerDroid = f;
-console.log(pricePerDroid);
+makeMessage('Radar', 6150);
+makeMessage('Scanner', 3500);
+makeMessage('Reactor', 8000);
+makeMessage('Engine', 4070);
