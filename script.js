37894d217b31044005ea8c12679d20369c796b6e
Losev Vladyslav <135152001+Obbjh@users.noreply.github.com>
2023-11-11 16:54:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-const products = [
-  { name: "Radar", price: 1300, quantity: 4 },
-  { name: "Scanner", price: 2700, quantity: 3 },
-  { name: "Droid", price: 400, quantity: 7 },
-  { name: "Grip", price: 1200, quantity: 9 },
-];
-
-function getAllPropValues(propName) {
-  // Change code below this line
-  const propValues = [];
-  for (const product of products) {
-    if (propName in product) {
-      propValues.push(product[propName]);
-    }
-  }
-  return propValues;
-}
+console.log(`hello`)
